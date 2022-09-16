840045bdab735d02392feff0023b89f553b2a427
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-16 11:09:20 -0700

    Styles header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return(
     <div className="header">
-      <h1 className="app-header">MK8 Deluxe Draft</h1>
+      <h1 className="app-header">MK8 DELUXE DRAFT</h1>
     </div>
   );
 };
