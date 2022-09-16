b20de5792632510a07c1219c6f9e9332647d4259
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-16 13:54:08 -0700

    Introduces state to start screen to conditionally render input fields

--- src/pages/StartScreen.jsx
+++ src/pages/StartScreen.jsx
@@ -3,69 +3,89 @@ import { useNavigate } from "react-router-dom";
 import "./StartScreen.css"
 
 const StartScreen = () => {
-  // const [players, setPlayers] = useState({
-  //   display: false,
-  //   number: 0,
-  //   order: "",
-  // });
+  const [display, setDisplay] = useState(false);
 
-  // const handleChange = (event) => {
-  //   setPlayers({
-  //     ...players,
-  //     [event.target.name]: event.target.value
-  //   });
-  // }; 
+  const [numberInput, setNumber] = useState({
+    number: "",
+  })
 
-  // const togglePlayers = () => {
-    
-  // }
+  const [players, setPlayers] = useState()
+
+  const handleChange = (event) => {
+    setNumber({
+      ...numberInput,
+      [event.target.name]: event.target.value
+    });
+  }; 
+
+  const handlePlayers = (event) => {
+    event.preventDefault()
+    toggleDisplay()
+    setPlayers(numberInput)
+  }
+
+  const toggleDisplay = () => {
+    if (display === false) {
+      setDisplay(true)
+    } else {
+      setDisplay(false)
+    }
+  }
+
 
   return(
     <div className="startscreen-wrapper">
-      <form>
-        <div className="draft-setup-questions">
-          <div className="how-many-players-wrapper">
-              <label htmlFor="players">How many players are in your lobby?</label>
-              <input
-                className="players-input"
-                type="number"
-                min="1"
-                max="12"
-                // value={players.number}
-                required={true}
-                // onChange={togglePlayers}
-              />  
+      {display === false 
+        ? <form onSubmit={handlePlayers}>
+          <div className="draft-setup-questions">
+            <div className="how-many-players-wrapper">
+                <label htmlFor="players">How many players are in your lobby?</label>
+                <input
+                  className="players-input"
+                  type="number"
+                  min="2"
+                  max="12"
+                  maxLength="2"
+                  name="number"
+                  value={numberInput.number}
+                  required={true}
+                  onChange={handleChange}
+                />  
+            </div>
+          <input type="submit" className="letsa-go" value="Letsa Go!"/>
           </div>
+        </form>
+      : <div className="conditional-wrapper">
         <div className="players-wrapper-headers">
-          <p>Name</p>
+          <p className="name-label">Type in players names in order of first place to last place</p>
         </div>  
         <div className="players-wrapper">
-          <div class="players-name-placement">
+          <div className="players-name-placement">
             <input
               className="player-name"
               type="text"
             /> 
           </div>
-          <div class="players-name-placement">
+          <div className="players-name-placement">
             <input
               className="player-name"
               type="text"
             />  
           </div>
-          <div class="players-name-placement">
+          <div className="players-name-placement">
             <input
               className="player-name"
               type="text"
             />   
           </div>
-          <div class="players-name-placement">
+          <div className="players-name-placement">
             <input
               className="player-name"
               type="text"
             />  
           </div>
+          <p className="more-players" onClick={() => toggleDisplay()}>Wait, more players showed up.</p>
         </div>   
-        {/* <input type="submit" className="submit" value="Submit"></input> */}
         <div className="placement-options-wrapper">
             <p className="placement-options-text">Draft Options</p>
             <div className="placement-options">
@@ -75,7 +95,7 @@ const StartScreen = () => {
             </div>
           </div> 
         </div>
-      </form>
+      }
     </div>
   );
 };
