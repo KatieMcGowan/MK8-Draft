e438fdd679c15cfc64dbc471bdbbfa963d22e8c6
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-15 17:05:02 -0700

    Populates results page with dummy data

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,7 +1,51 @@
 const Results = () => {
   return(
-    <div>
-      <h1>Results</h1>
+    <div className="results-wrapper">
+      <p className="results-header">Results</p>
+      <div className="results-container">
+        <div className="player-result">
+          <p>Becky</p>
+          <div className="player-build">
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="character">Yoshi</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="kart">Biddybuggy</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="wheels">Azure Roller</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="glider">Flower Glider</p>
+            </div>
+          </div>
+        </div>
+        <div className="player-result">
+          <p>Becky</p>
+          <div className="player-build">
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="character">Yoshi</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="kart">Biddybuggy</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="wheels">Azure Roller</p>
+            </div>
+            <div className="selected">
+              <div className="results-placeholder"></div>
+              <p className="glider">Flower Glider</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
